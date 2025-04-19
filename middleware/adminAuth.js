8ee9352b49cd6785917390c163ead36c4ce26a4b
jaynguyen1510 <ngnhatnam1510@gmail.com>
2025-04-20 02:06:10 +0700

    fix adminAuth

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,16 +1,22 @@
 const jwt = require("jsonwebtoken");
+
 const adminAuth = async (req, res, next) => {
   try {
-    const { token } = req.headers;
-    if (!token) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
       return res.status(401).json({ success: false, message: "Unauthorized" });
     }
+
+    const token = authHeader.split(" ")[1];
+
     const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
     if (tokenDecoded.email !== process.env.ADMIN_EMAIL) {
       return res
         .status(401)
-        .json({ success: false, message: "not authorized" });
+        .json({ success: false, message: "Not authorized" });
     }
+
     next();
   } catch (error) {
     console.log(error);
